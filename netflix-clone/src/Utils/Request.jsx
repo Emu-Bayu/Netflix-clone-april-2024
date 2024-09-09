500bd3e8047dfb4a77002ae6da5be97f7bde8b60
Emu Bayu <emebetbayu19@gmail.com>
2024-09-09 02:35:29 -0400

    banner

--- netflix-clone/src/Utils/Request.jsx
+++ netflix-clone/src/Utils/Request.jsx
@@ -1,9 +1,12 @@
-const API_KEY=process.env.REACT_APP_API_KEY;
+const API_KEY = import.meta.env.VITE_API_KEY;
+
 const request={
-  fetchtvmovies: `/discover/tv?api_key=${API_KEY}&with_networks-21`,
+  fetchtranding :`/trending/tv/day?api_key=${API_KEY}&language=en-US`,
+   fetchnetflixorginals: `/discover/tv?api_key=${API_KEY}&with_networks-21`,
   fetchcomedymovies:`/discover/movie?api_key=${API_KEY}&with_genres=35`, 
  fetchactionmovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`, 
   fetchromancemovies:`/discover/movie?api_key=${API_KEY}&with_genres=10749`, 
   fetchTopRatedMovies:`/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1
 `, 
 }
+export default request
---
+++ netflix-clone/src/components/Banner/Banner.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react'
+import axios from "../../Utils/Axios"
+import request from "../../Utils/Request"
+import "./banner.css"
+
+function Banner() {
+const [movie, setmovies]= useState({});
+useEffect(()=>{
+    ( async()=>{
+    try{const response= await axios.get(request.fetchnetflixorginals)
+        console.log(response)
+        setmovies(response.data.results[
+            Math.floor(Math.random() *response.data.results.length)
+        ]);console.log(setmovies)
+    }catch(error){
+        console.log("error",error)
+    }
+})()
+   
+},[])
+
+function trancate(string,n) {
+  return string?.length> n ? string.substring(0,n)+"..." :string 
+  
+}
+
+  return (
+    <div className='banner'
+    style={{
+        backgroundSize:"cover",
+        backgroundImage:`url('https://image.tmdb.org/t/p/original/${movie?.backdrop_path}')`,
+        backgroundPosition:"center",
+        backgroundRepeat:"no-repeat"
+    }}
+>
+          <div className='banner_container'>
+            <h1 className='banner_title'>
+                {movie?.title||movie.name||movie.original_name}</h1>
+              <div className='banner_buttons'>
+                <button className='banner_button play'>Play</button>
+                <button className='banner_button'>My List</button>
+              </div>
+              <h3 className='banner_discription'> { trancate(movie?.overview ,150)}</h3>
+          </div>
+          <div className='banner_fadebutton'/>
+    </div>
+  )
+}
+export default Banner
